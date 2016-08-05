3ec01035e38704fd1fb0df1dad4ef083189027c1
Calvin Lee <lwd369@gmail.com>
2016-08-05 18:26:58 +0800

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   end
 
   def help
+    puts "clllllllllllllllllciked HHHHHHHHHHHHEEEEEEEEEEEEEEEEEELLLLLLLLLLLLLLLLLLPPPPPPPPPPPPPPPPPP"
   end
 
   def about
